ba4ebb2750aec85f26b434b7bef4f9c3d5ed33bf
Rupert Nash <rupert@nash.me.uk>
2019-02-08 16:20:41 +0000

    add lectures

---
+++ lectures/README.md
@@ -0,0 +1,6 @@
+# Lectures
+
+* [Lecture 1](01) - A brief introduction to C++
+* [Lecture 2](02) - C++ for numerical computing 1
+* [Lecture 3](03) - C++ for numerical computing 2
+* [Lecture 4](04) - C++ frameworks for portable performance
---
+++ lectures/template/cpptheme.js
@@ -0,0 +1,12 @@
+
+cpptheme = new Theme(
+    (str => str.substring(0, str.lastIndexOf("/")))(document.currentScript.src),
+    '$BASEURL/style.css',
+    {
+	thumb: function () {
+	    return '.thumb[\n.thumbtxt[\n' + this +'\n]\n]';
+	},
+	footer: function() {
+	    return epcc.footer() + "<p>&copy; Rupert Nash, The University of Edinburgh, CC-BY</p>";
+	}
+    });
